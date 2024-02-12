6395d635095db3093b7395d3cbfbf17a11466b1d
Denis Saveliev <denissaveliev23@gmail.com>
2024-02-12 21:56:34 +0200

    feat: solve sortDigitNamesByNumericOrder task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -573,8 +573,30 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const namesOfDigit = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  const digitsArr = arr
+    .map((item) => {
+      return namesOfDigit[item];
+    })
+    .sort()
+    .map((digit) => {
+      return Object.keys(namesOfDigit).find(
+        (key) => namesOfDigit[key] === digit
+      );
+    });
+  return digitsArr;
 }
 
 /**
